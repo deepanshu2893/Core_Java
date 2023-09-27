47a06d30b327b44cb672141e5f12398ce2967310
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-09-27 18:46:55 +0530

    Multithreading

---
+++ Multithreading_In_Java/MultithreadThing.java
@@ -0,0 +1,17 @@
+package Multithreading_In_Java;
+
+public class MultithreadThing extends Thread{
+
+    @Override
+    public void run(){
+        for (int i = 1; i <= 5; i++){
+            System.out.println(i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
---
+++ Multithreading_In_Java/Multithreading.java
@@ -0,0 +1,10 @@
+package Multithreading_In_Java;
+
+public class Multithreading {
+
+    public static void main(String[] args) {
+
+        Multithreading myThing = new Multithreading();
+        myThing.start();  
+    }
+}
