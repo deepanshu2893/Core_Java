53593846a9984dfd734689503ce9cede590aea18
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-09-28 22:34:13 +0530

    Update MultithreadThing.java

--- Multithreading_In_Java/MultithreadThing.java
+++ Multithreading_In_Java/MultithreadThing.java
@@ -4,7 +4,7 @@ public class MultithreadThing extends Thread{
 
     @Override
     public void run(){
-        for (int i = 1; i <= 5; i++){
+        for (int i = 1; i < 5; i++){
             System.out.println(i);
             try {
                 Thread.sleep(1000);
