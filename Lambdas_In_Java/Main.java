ab55ef5c74077d61462bdd2bded1bcb585156078
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-03-22 23:36:49 +0530

    Update Main.java

--- Lambdas_In_Java/Main.java
+++ Lambdas_In_Java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         helloPrint(
                 () -> {
-                    System.out.println("hello World!");
+                    System.out.println("hello World");
                 }
         );
     }
